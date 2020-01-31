3f71eafa9f110764467e1394d81eccb4e15320c8
abeforgit <arnebertrand@gmail.com>
2020-02-01 00:52:01 +0100

    Start with clickdrag

---
+++ GGJ2020/Assets/ClickDragTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDragTest : MonoBehaviour
+{
+    public SpringJoint2D spring;
+
+    float distance = 10;
+
+    private void OnMouseDrag()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        transform.position =mousePos;
+    }
+}
