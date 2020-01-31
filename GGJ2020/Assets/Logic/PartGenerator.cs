555132418e04635a34a30e1fb526757bc5017e7d
Arne Jacobs <arne.jacobs@ugent.be>
2020-02-01 00:47:58 +0100

    added PartGenerator

---
+++ GGJ2020/Assets/Logic/PartGenerator.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using Random = System.Random;
+
+namespace Logic
+{
+    public class PartGenerator
+    {
+        private static Random rnd = new Random();
+
+        private static float NextFloat()
+        {
+            double u1 = 1.0-rnd.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0-rnd.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                                   Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+            double randNormal = Math.Abs((randStdNormal)); 
+            return (float) (randNormal* 100);
+        }
+
+        public static Part GeneratePart()
+        {
+            Part newPart = new Part();
+
+            int maxValue = (int)Enum.GetValues(typeof(PartType)).Cast<PartType>().Max();
+            newPart.Type = (PartType) rnd.Next(maxValue + 1);
+            newPart.Description = "test description"; //TODO
+            newPart.Name = newPart.Type.ToString(); //TODO
+            newPart.Stats = new Stats()
+            {
+                Speed = NextFloat(),
+                Vitality = NextFloat(),
+                Intelligence = NextFloat(),
+                Strength = NextFloat(),
+                Dexterity = NextFloat(),
+                Charisma = NextFloat(),
+                Durability = NextFloat(),
+            };
+            return newPart;
+        }
+    }
+}
