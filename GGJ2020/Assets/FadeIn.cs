430845040e20ce71e03e1ae35d31788d147577ce
Jimmy van den Bergh <j.vd.bergh@split-polygon.com>
2020-02-01 20:59:48 +0100

    - Title screen LOL

---
+++ GGJ2020/Assets/FadeIn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    Texture2D blk;
+    public bool fade = false;
+    public float alph;
+    void Start()
+    {
+        //make a tiny black texture
+        blk = new Texture2D(1, 1);
+        blk.SetPixel(0, 0, new Color(0, 0, 0, 1));
+        blk.Apply();
+        alph = 1;
+        StartCoroutine(waiter());
+    }
+    // put it on your screen
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), blk);
+    }
+
+    void Update()
+    {
+        if (fade == true)
+        {
+            if (alph > 0)
+            {
+                alph -= Time.deltaTime * .1f;
+                if (alph < 0) { alph = 0f; }
+                blk.SetPixel(0, 0, new Color(0, 0, 0, alph));
+                blk.Apply();
+            }
+        }
+    }
+
+    IEnumerator waiter()
+    {
+        //Wait for 2 seconds
+        yield return new WaitForSeconds(2);
+        fade = true;
+    }
+}
