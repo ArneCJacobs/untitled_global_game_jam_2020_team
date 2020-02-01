c0d895483f4a20a09028c8c334a0a7c3932cdb91
Hurles <j.meulenkamp@split-polygon.com>
2020-02-02 00:21:01 +0100

    - added body object to bodies

---
+++ GGJ2020/Assets/BodyVisual.cs
@@ -0,0 +1,23 @@
+﻿using Logic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyVisual : MonoBehaviour
+{
+    Body Body;
+
+    List<SnappingPoint> SnapList = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var item in GetComponentsInChildren<SnappingPoint>())
+            Body.Slots.Add(item);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
