1957e0b2abc5cb7abead862233141c39dff91344
Jimmy van den Bergh <j.vd.bergh@split-polygon.com>
2020-02-01 16:12:24 +0100

    - audio changes, background music and conveyor

---
+++ GGJ2020/Assets/AudioHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AudioHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<AudioSource> AudioSources = new List<AudioSource>();
+    float FadeInTime = 0;
+    float MaxFadeInTime = 5.0f;
+    void Start()
+    {
+        AudioSources = this.GetComponentsInChildren<AudioSource>().ToList();
+            
+        foreach ( var audioSource in AudioSources)
+        {
+            audioSource.volume = 0.0f;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FadeInTime < MaxFadeInTime)
+        {
+            float factor = FadeInTime / MaxFadeInTime;
+            FadeInTime += Time.deltaTime;
+            foreach (var audioSource in AudioSources)
+            {
+                audioSource.volume = Mathf.Lerp(0, 1.0f, factor);
+            }
+        }
+
+    }
+}
